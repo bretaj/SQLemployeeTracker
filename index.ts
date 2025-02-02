e78ce7b03a5d277a41777a9979dcec3ea8c83610
bret jensen <jensenbret13@gmail.com>
2025-02-02 12:38:28 -0800

    .

--- index.ts
+++ index.ts
@@ -19,4 +19,74 @@ function mainMenu(): void {
                 'Exit'
             ]
         }
+    ]
+};    
+
+function addDepartment(): void {
+    const questions: QuestionCollection = [
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'What is the name of the department?'
+        }
     ];
+    // todo: code to add new dept to db
+}
+
+function addRole(): void {
+    const questions: QuestionCollection = [
+        {
+            type: 'input',
+            name: 'roleName',
+            message: 'What is the title of the role?'
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the salary for this role?',
+        },
+        {
+            type: 'input',
+            name: 'departmentId',
+            message: 'What is the department ID for this role?',
+        }
+    ];
+        // todo: code to add new role to db
+}
+
+function addEmployee(): void {
+    const questions: QuestionCollection = [
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'What is the first name of the employee?',
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the last name of the mployee?',
+        },
+        {
+            type: 'input',
+            mame: 'roleID',
+            message: 'What is the role ID for this employee?',
+        },
+        {
+            type: 'input',
+            name: 'managerID',
+            message: 'What is the manager ID for this employee (leae blank if none)?',
+            default: null
+        }
+    ];
+    // todo: code to add new employee to db
+}
+
+function updateEmployee(): void {
+    const questions: QuestionCollection = [
+        {
+            type: '',
+            name: '',
+            message: '',
+        }
+    ]
+}
